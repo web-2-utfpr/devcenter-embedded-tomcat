1e7cbfa70780aa42eb2b6f76f0a5a9112b64949f
Rafael Santos <rafaelnsantos1@gmail.com>
2018-04-11 23:47:16 -0300

    fix

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet(
         name = "MyServlet",
         urlPatterns = {"/hello"}
@@ -24,13 +23,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        try {
-            System.out.println(Conexao.teste());
-        } catch (SQLException ex) {
-            Logger.getLogger(HelloServlet.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (NamingException ex) {
-            Logger.getLogger(HelloServlet.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        System.out.println(Conexao.teste());
         ServletOutputStream out = resp.getOutputStream();
         out.write("hello heroku".getBytes());
         out.flush();
