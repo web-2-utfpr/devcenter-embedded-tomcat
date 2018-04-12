fdb2e9f1dd573a493c1fe7cbfc5dfb8aee24e567
Rafael Santos <rafaelnsantos1@gmail.com>
2018-04-12 03:10:51 -0300

    clean

--- src/main/java/servlet/ImageServlet.java
+++ src/main/java/servlet/ImageServlet.java
@@ -3,7 +3,6 @@ package servlet;
 import dao.ImageDAO;
 import entities.User;
 import java.io.IOException;
-import javax.servlet.RequestDispatcher;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -28,9 +27,25 @@ public class ImageServlet extends HttpServlet {
             return;
         }
         
+        req.setAttribute("user", loggedUser);
         req.setAttribute("images", ImageDAO.getPhotos(loggedUser));
-        
         req.getRequestDispatcher("/logged.jsp").forward(req, resp);
+    }
+    
+     @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        
+        User loggedUser = (User) req.getSession().getAttribute("loggedUser");
+        
+        if (loggedUser == null) {
+            resp.sendRedirect("login");
+            return;
+        }
+        
+        // UPAR IMAGEM
+        
+        // ADICIONAR NO BANCO
     }
 
 }
