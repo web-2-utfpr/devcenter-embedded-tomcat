858bb572ee38d1cf99bff019c878a7315abbcd30
LucasSantosJr <lucassantosjunior@gmail.com>
2018-05-22 20:48:49 -0300

    #61 #55 #52 #29  #27

--- src/main/java/model/service/UsuarioService.java
+++ src/main/java/model/service/UsuarioService.java
@@ -1,14 +1,17 @@
 package model.service;
 
+import exception.InvalidPasswordException;
+import exception.UserAlreadyExistsException;
+import exception.UserNotFoundException;
 import model.entity.User;
 import org.javalite.activejdbc.LazyList;
 import org.javalite.activejdbc.Model;
 
 public class UsuarioService {
 
-    public static void registrar(String nome, String email, String senha) throws Exception {
+    public static void registrar(String nome, String email, String senha) throws UserAlreadyExistsException {
         if (User.findFirst("nome = ? OR email = ?", nome, email) != null) {
-            throw new Exception("User not found");
+            throw new UserAlreadyExistsException();
         }
 
         User user = new User();
@@ -18,11 +21,11 @@ public class UsuarioService {
         user.save();
     }
 
-    public static Model login(String nome, String senha) throws Exception {
+    public static Model login(String nome, String senha) throws InvalidPasswordException, UserNotFoundException {
         Model user = findByUsername(nome);
 
         if (!user.getString("senha").equals(senha)) {
-            throw new Exception("Wrong password");
+            throw new InvalidPasswordException();
         }
 
         return user;
@@ -33,10 +36,10 @@ public class UsuarioService {
         return User.where("nome LIKE ?", "%" + username + "%");
     }
 
-    public static Model findByUsername(String username) throws Exception {
+    public static Model findByUsername(String username) throws UserNotFoundException {
         Model user = User.findFirst("nome = ?", username);
         if (user == null) {
-            throw new Exception("User not found");
+            throw new UserNotFoundException();
         }
         return user;
     }
