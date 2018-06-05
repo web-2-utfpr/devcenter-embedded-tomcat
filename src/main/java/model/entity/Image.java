6374eba32752be6c10555509e102d6f2adfe7d9f
JosielFaleiros <josiel.2015@alunos.utfpr.edu.br>
2018-06-05 19:51:51 -0300

    removing the message #71

--- src/main/java/model/entity/Image.java
+++ src/main/java/model/entity/Image.java
@@ -23,24 +23,7 @@ public class Image extends Model {
     }
 
     public String getCreate_time() {
-        Duration d = Duration.between(getTimestamp("created_at").toLocalDateTime(), LocalDateTime.now());
-
-        if (d.toMillis() < 60000) {
-            return d.toMillis() / 1000 + " segundos atras";
-        }
-        if (d.toMinutes() < 60) {
-            return d.toMinutes() + (d.toMinutes() == 1 ? " minuto atras" : " minutos atras");
-        }
-        if (d.toHours() < 24) {
-            return d.toHours() + (d.toHours() == 1 ? " hora atras." : " horas atras");
-        }
-        if (d.toDays() < 7) {
-            return d.toDays() + (d.toDays() == 1 ? " dia atras." : " dias atras");
-        }
-        if (d.toDays() < 365) {
-            return d.toDays() / 7 + (d.toDays() / 7 == 1 ? " semana atras." : " semanas atras");
-        }
-        return d.toDays() / 365 + (d.toDays() / 365 == 1 ? " ano atras." : " anos atras");
+        return getTimestamp("created_at").toLocalDateTime();
     }
 
     public String getUsuario() {
