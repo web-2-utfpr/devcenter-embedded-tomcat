5497055da6b0ceb2c8618af32356640e573f88a5
Rafael Santos <rafaelnsantos1@gmail.com>
2018-06-19 21:12:50 -0300

    cors fix

--- src/main/java/cors/CORSFilter.java
+++ src/main/java/cors/CORSFilter.java
@@ -5,24 +5,28 @@
  */
 package cors;
 
-import java.io.IOException;
-
+import javax.inject.Singleton;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
 
-public class CORSFilter
-implements ContainerResponseFilter {
+@Singleton
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
 
-	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
-			throws IOException {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        final int ACCESS_CONTROL_MAX_AGE_IN_SECONDS = 12 * 60 * 60;
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
 
-		MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+        headers.add("ACCESS_CONTROL_ALLOW_ORIGIN", "*");
+        headers.add("ACCESS_CONTROL_ALLOW_HEADERS", "origin, content-type, accept, authorization");
+        headers.add("ACCESS_CONTROL_ALLOW_CREDENTIALS", "true");
+        headers.add("ACCESS_CONTROL_ALLOW_METHODS", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        headers.add("ACCESS_CONTROL_MAX_AGE", ACCESS_CONTROL_MAX_AGE_IN_SECONDS);
 
-		headers.add("Access-Control-Allow-Origin", "*");
-		headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");			
-		headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Codingpedia");
-	}
-
+    }
 }
