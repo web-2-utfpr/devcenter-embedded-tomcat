b2b85cde21285aaff1e7e499a33b7ed4c4e99522
Rafael Santos <rafaelnsantos1@gmail.com>
2018-06-21 16:52:34 -0300

    cors

---
+++ src/main/java/cors/CORSResponseFilter.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cors;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CORSResponseFilter implements Filter {
+
+    public CORSResponseFilter() {
+
+    }
+
+    @Override
+    public void destroy() { 
+        // TODO Auto-generated method stub
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String method = ((HttpServletRequest) request).getMethod();
+        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Origin", "*");
+        if (method.equalsIgnoreCase("OPTIONS")) {
+            ((HttpServletResponse) response).addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+            ((HttpServletResponse) response).addHeader("Access-Control-Allow-Headers", "origin, accept,content-type,authorization");
+            ((HttpServletResponse) response).addHeader("Vary", "Origin");
+            ((HttpServletResponse) response).addHeader("Access-Control-Max-Age", "1728000");
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+        // TODO Auto-generated method stub
+    }    
+}
