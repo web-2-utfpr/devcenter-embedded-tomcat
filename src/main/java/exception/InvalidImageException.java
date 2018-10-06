64c561da93f95fdf24acfad62c6cf2754b17d92b
Rafael Santos <rafaelnsantos1@gmail.com>
2018-10-06 16:54:11 -0300

    add exception

---
+++ src/main/java/exception/InvalidImageException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class InvalidImageException extends BaseException {
+
+    public InvalidImageException() {
+        super(messages.getString("invalidImage"));
+    }
+    
+}
